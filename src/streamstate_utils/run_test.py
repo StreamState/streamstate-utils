f30d9c2236af73cc46aa0202c0298a4f44733536
Daniel Stahl <danstahl1138@gmail.com>
2021-04-20 15:20:31 -0500

    added explicit import

--- src/streamstate_utils/run_test.py
+++ src/streamstate_utils/run_test.py
@@ -68,10 +68,10 @@ def helper_for_file(
 
 
 def main():
+    from process import process
+
     [name, path_to_process, path_to_inputs, path_to_outputs] = sys.argv
     input_schema = marshmallow_dataclass.class_schema(InputStruct)()
-    with open(path_to_process) as f:
-        exec(f.read())  # exports process as function
     with open(path_to_inputs) as f:
         inputs = [input_schema.load(v) for v in json.load(f)]
     with open(path_to_outputs) as f:
