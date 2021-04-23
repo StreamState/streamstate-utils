f73fbed51d35b8c95cee3f89677700f1c0ffe1a6
Daniel Stahl <danstahl1138@gmail.com>
2021-04-23 17:54:31 -0500

    add path to key

--- src/streamstate_utils/gcs_create_folder.py
+++ src/streamstate_utils/gcs_create_folder.py
@@ -3,15 +3,20 @@ from streamstate_utils.utils import get_folder_location
 from streamstate_utils.structs import InputStruct
 from typing import List
 import google.auth
+from google.oauth2 import service_account
 
 
 def create_gcs_folders(
+    path_to_json_key: str,
     bucket_name: str,
     app_name: str,
     inputs: List[InputStruct],
 ):
-    credentials, project = google.auth.default()
-    gcs_client = storage.Client(project=project, credentials=credentials)
+    # credentials, project = google.auth.default()
+    credentials = service_account.Credentials.from_service_account_file(
+        path_to_json_key
+    )
+    gcs_client = storage.Client(pcredentials=credentials)
     bucket = gcs_client.get_bucket(bucket_name)
     for input in inputs:
         folder_name = get_folder_location(app_name, input.topic)
